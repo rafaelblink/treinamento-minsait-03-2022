cff3ff225fde338763c0a24c0ce15536e873b5ba
Rafael Ferreira <rafaelblink@gmail.com>
2023-03-28 16:30:40 -0300

    improve: implements pages and routers

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { LivrosComponent } from './pages/livros/livros.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home', component: HomeComponent
+  },
+  {
+    path: 'livros', component: LivrosComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
